abd0e3e5ab12eabd0daefaf5782b405f821e1073
Leonardo Barbosa <44064137+barbosaleonardo@users.noreply.github.com>
2025-03-19 12:13:01 -0300

    correcao para atividade final na infra

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,10 +2,14 @@ variable "cluster_name" {
   description = "Nome do cluster ECS"
   type        = string
 }
+
+
 variable "subnets_id" {
   description = "Subnets IDs"
   type        = list(string)
 }
+
+
 variable "vpc_id" {
   description = "VPC ID"
   type        = string
